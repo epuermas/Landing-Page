ce945810b871c6a00f6afc0c53ac9d1302c6dd84
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-03 16:28:28 +0000

    minor cosmetic updates

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -4,9 +4,13 @@ import React from "react";
 const Card = () => {
 	return (
 		<div className="card" style={{ width: "18rem" }}>
-			<img src="..." className="card-img-top" alt="..." />
+			<img
+				src="https://image.shutterstock.com/image-vector/grunge-red-sample-word-round-260nw-1242668641.jpg"
+				className="card-img-top"
+				alt="..."
+			/>
 			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
+				<h5 className="card-title">Sample Card Title</h5>
 				<p className="card-text">
 					Some quick example text to build on the card title and make
 					up the bulk of the cards content.
