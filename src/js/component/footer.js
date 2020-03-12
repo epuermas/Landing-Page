7fd792b389e77936a5773e7736a4f987021b441d
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-12 19:29:08 +0000

    footer added - students example

---
+++ src/js/component/footer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export const Footer = () => (
+	<footer className="footer mt-auto py-2 text-center">
+		<p className="footer-text">
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a
+				href="https://www.instagram.com/eddy_prms/"
+				target="_blank"
+				rel="noopener noreferrer">
+				Eduardo Puermas
+			</a>
+		</p>
+		<div className="footer-icons align-self-center">
+			<a
+				className="fab fa-linkedin"
+				a
+				href="https://www.linkedin.com/in/eduardopuermas8698/"
+				id="linkedin-footer"
+				target="_blank"
+				rel="noopener noreferrer"
+			/>
+			<a
+				className="fab fa-github-square"
+				a
+				href="https://github.com/epuermas"
+				id="github-footer"
+				target="_blank"
+				rel="noopener noreferrer"
+			/>
+		</div>
+	</footer>
+);
+export default Footer;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
 import Card from "./cards";
+import Footer from "./footer";
 
 const Home = () => {
 	return (
@@ -14,6 +15,7 @@ const Home = () => {
 				<Card />
 				<Card />
 			</div>
+			<Footer />
 		</div>
 	);
 };
